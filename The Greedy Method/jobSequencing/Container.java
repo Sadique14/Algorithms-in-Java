cbb4ebe3ce91e483762f7c2b796d6b2079c1c783
Muhammad Farhan Sadique <md.farhansadique14@gmail.com>
2015-12-27 22:39:10 +0600

    Revert "Algorithms codes in java. I did these code during my Algorithms course (2nd year 2nd term, KU)"
    This reverts commit eef9b191a05eeea6f04707143c630589880dc410.

--- The Greedy Method/jobSequencing/Container.java
+++
@@ -1,8 +0,0 @@
-package jobSequencing;
-
-public class Container {
-
-	public int p;
-	public int d;
-	public int s;
-}
--- The Greedy Method/knapsackProblem/Container.java
+++
@@ -1,9 +0,0 @@
-package knapsackProblem;
-
-public class Container {
-
-	int p;
-	int w;
-	int sortingVariable;
-	float x;
-}
